54a27d565575bfec1a12c3670c735b006aa7efa0
mpdomon <martin.domon@gmail.com>
2024-03-05 20:45:25 +0100

    Add project files.

---
+++ Lexicon-uppgift2/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+while (true)
+{
+    Console.WriteLine("This is the main menu, please use the following numbers to navigate and test different functions");
+    Console.WriteLine("" +
+        "Press 0 to exit.\n " +
+        "Press 1 to check your 'youthfulness' lol.\n " +
+        "Press 2 to repeat 10 times.\n" +
+        "Press 3 to get the third word of whatever sentence you enter in the next step." 
+        );
+     
+
+    /* Menyval 1: Ungdom eller pensionär
+     * Menyval 2: Upprepa tio gånger
+     * Menyval 3: Det tredje ordet
+     */
+}
+
+
